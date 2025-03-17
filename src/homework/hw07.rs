e15bfb38b93b7bd9777dce45ea1402be0f3608ab
rivalmans <rivalmansonq@gmail.com>
2025-03-17 20:34:54 +0100

    Update hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,39 @@
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_lowercase().to_string()
+            } else {
+                c.to_uppercase().to_string()
+            }
+        })
+        .collect()
+}
+
+#[test]
+fn test() {
+    let data =
+        [
+            ("Hello", "hELLO"),
+            ("Привет", "пРИВЕТ"),
+        ];
+
+    data
+        .iter()
+        .for_each(|(a, b)| {
+            assert_eq!(
+                invert_the_case(a.to_string()),
+                b.to_string()
+            );
+            assert_eq!(
+                invert_the_case(b.to_string()),
+                a.to_string()
+            );
+        });
+}
+
+fn main() {
+    let input = "somethinG, SOMETHINg".to_string();
+    let output = invert_the_case(input);
+    println!("{}", output);
+}
