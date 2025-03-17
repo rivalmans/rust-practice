df9c0c1831d31d692f7645865f00b05cf86b8cff
rivalmans <rivalmansonq@gmail.com>
2025-03-17 20:58:32 +0100

    Update hw08.rs

---
+++ src/homework/hw08.rs
@@ -0,0 +1,40 @@
+fn is_prime(n: &u32) -> bool {
+    if *n < 2 {
+        return false;
+    }
+    for i in 2..=((*n as f64).sqrt() as u32) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+#[test]
+fn test_is_prime() {
+    let test_data = [
+        (0, false),
+        (1, false),
+        (2, true),
+        (3, true),
+        (4, false),
+        (5, true),
+        (100, false),
+        (10007, true),
+    ];
+
+    test_data
+        .iter()
+        .for_each(|(n, prime)|
+            assert_eq!(is_prime(n), *prime)
+        )
+}
+
+fn main() {
+    let num = 29;
+    if is_prime(&num) {
+        println!("Число {} є простим", num);
+    } else {
+        println!("Число {} не є простим", num);
+    }
+}
